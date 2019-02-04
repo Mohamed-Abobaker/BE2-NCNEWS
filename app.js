8c962b8e7a5af409fd9a1aa2f512c6728b38f12a
Mohamed-Abobaker <mohamedabobaker17@gmail.com>
2019-02-04 15:46:12 +0000

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const app = require('express')();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
 const bodyParser = require('body-parser');
+
+app.use(cors());
+
 const apiRouter = require('./routes/api');
 const {
   handle400, handle404, handle422, handle500,
 } = require('./errorHandling/errorHandling');
 
 app.use(bodyParser.json());
+
 app.use('/api', apiRouter);
 
 app.use(handle400);
